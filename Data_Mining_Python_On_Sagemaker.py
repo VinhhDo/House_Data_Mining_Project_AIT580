ef6b361b77e138bbe25464a7b6ff70d636a8407f
VinhhDo <98499217+VinhhDo@users.noreply.github.com>
2024-01-02 16:30:31 -0500

    Update Data_Mining_Python_On_Sagemaker.py

--- Data_Mining_Python_On_Sagemaker.py
+++ Data_Mining_Python_On_Sagemaker.py
@@ -1,3 +1,52 @@
+import pandas as pd
+import boto3
+import requests
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib as mpl
+import seaborn as sns
+from matplotlib.ticker import FuncFormatter
+from matplotlib.ticker import ScalarFormatter
+import matplotlib.ticker as ticker
+import uuid
+
+pd.set_option('display.max_columns', None)
+warnings.filterwarnings('ignore')
+
+# Convert 'sale_price' column to float
+merged_df['sale_price'] = merged_df['sale_price'].astype(float)
+
+# Convert 'building_year_built' column to datetime
+merged_df['building_year_built'] = pd.to_datetime(merged_df['building_year_built'], errors='coerce')
+
+# Create a new 'date' column by combining 'month' and 'year' columns
+merged_df['date'] = pd.to_datetime(merged_df[['year', 'month']].assign(day=1))
+
+# Drop the original 'month' and 'year' columns if needed
+merged_df = merged_df.drop(['month', 'year'], axis=1)
+
+# Assuming merged_df is your DataFrame
+merged_df['squarefeet'] = merged_df['sale_price'] / merged_df['median_ppsf']
+
+# Assuming merged_df is your DataFrame
+merged_df['num_rooms'] = (merged_df['squarefeet'] / 500).apply(np.ceil).fillna(0).astype(int)
+room_mapping = {1:1,2:2,3:3,4:4,5:5,6:5,7: 5, 8: 6, 9: 6, 10: 7, 11: 7, 12: 8, 13: 7}
+
+# Apply the mapping to the 'num_rooms' column
+merged_df['num_rooms'] = merged_df['num_rooms'].map(room_mapping)
+merged_df['building_year_built'] = pd.to_datetime(merged_df['building_year_built'], errors='coerce')
+
+# Define the bins
+bins = [float('-inf'), 1950, 1960, 1970, 1980, 1990, 2000, 2010, 2022, float('inf')]
+
+# Create the build_period column
+merged_df['build_period'] = pd.cut(merged_df['building_year_built'].dt.year, bins=bins, right=False)
+
+# Convert the build_period column to object type
+merged_df['build_period'] = merged_df['build_period'].astype(str)
+# Filter out records with sale_price above 20,000,000 or below 10,000
+merged_df = merged_df[(merged_df['sale_price'] <= 20000000) & (merged_df['sale_price'] >= 10000)]
+
 # 1.1 What is the current median sale price for properties in the area?
 selected_states = ['NY', 'NJ', 'MD', 'VA', 'NC', 'FL', 'CA']
 filtered_df = merged_df[merged_df['state_code'].isin(selected_states)]
@@ -19,11 +68,6 @@ for i, value in enumerate(median_sale_price_by_state['median_sale_price']):
     plt.text(value, i, f'${value:.2f}', ha='left', va='center')
 
 plt.show()
-
-
-
-
-
 
 
 # 1.2 What is the current inventory level, and how does it compare to historical averages? (Supply)
